889b62254890c9c3e43345915feda45fe9dcd647
David Gaona <davidgaonahenao@gmail.com>
2024-05-31 11:40:04 -0500

    New modifications to change some letters in spanish to english

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -5,7 +5,7 @@ export const Home = () => {
   return (
     <div className="">
       <h1 className="m-2 text-5xl font-bold text-center text-blue-700">
-        Inicio
+        Start
       </h1>
       <div className="flex flex-col items-center justify-center p-8 m-4 w-120 h-240 bg-gradient-to-b from-violet-700 via-sky-600 to-red-700 rounded-xl">
         <Saludo />
